20663906fd288b7407046167f0b8b6dc11704c92
Joel Uzcategui Lorenzo <Joel Uzcategui Lorenzo>
2019-05-16 15:04:50 -0400

    Primer commit

---
+++ main.py
@@ -0,0 +1,106 @@
+#!/usr/bin/env python
+# -*- coding: utf-8 -*-
+#
+#  main.py
+#  
+#  Copyright 2019  <pi@raspberrypi>
+#  
+#  This program is free software; you can redistribute it and/or modify
+#  it under the terms of the GNU General Public License as published by
+#  the Free Software Foundation; either version 2 of the License, or
+#  (at your option) any later version.
+#  
+#  This program is distributed in the hope that it will be useful,
+#  but WITHOUT ANY WARRANTY; without even the implied warranty of
+#  MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+#  GNU General Public License for more details.
+#  
+#  You should have received a copy of the GNU General Public License
+#  along with this program; if not, write to the Free Software
+#  Foundation, Inc., 51 Franklin Street, Fifth Floor, Boston,
+#  MA 02110-1301, USA.
+#  
+#  
+import sys
+import sqlite3
+from tkinter import *
+
+
+class FullScreenWindow:
+    
+    def clicked(self,t):
+        self.admin = Toplevel(self.tk)
+        self.admin.title(t)
+        print(t)
+
+    def AdminVentana(self,t):
+    	self.framePrincipal.pack_forget()
+    	self.frameAdmin.pack()
+        
+        
+    def __init__(self):
+        self.tk = Tk()
+        self.tk.title("Soy un titulo")
+        self.tk.attributes("-zoomed", True)
+        self.tk.attributes("-fullscreen", False)
+        self.frameAdmin = Frame(self.tk)
+        self.labelAdmin = Label(self.tk, text="FRAME DE ADMIN")
+        self.labelAdmin.grid(column=0, row=0)
+       	#etiqueta = Tkinter.Label(root, text="Probando Label")
+        self.framePrincipal = Frame(self.tk)
+        #~ self.tk.configure (bg="green")
+        self.framePrincipal.configure (bg="#eaebf1")
+        self.framePrincipal.pack(fill=BOTH,expand=1)
+
+        self.btn1 = Button(self.framePrincipal, text="Admin", command=lambda:self.clicked("Admin") , height = 10, width = 10)
+        self.btn2 = Button(self.framePrincipal, text="User", command=lambda:self.clicked("User") , height = 10, width = 10)
+        self.btn1.grid(column=0, row=0, padx= 300, pady=100, sticky= "ewns")
+        self.btn2.grid(column=1, row=0, pady=100)
+        self.btn1.configure (bg="#f1f0ea")
+        self.btn2.configure (bg="#f1f0ea")
+        self.state = False
+        self.tk.bind("<F11>", self.toggle_fullscreen)
+        self.tk.bind("<Escape>", self.end_fullscreen)        
+    
+
+        
+    def toggle_fullscreen(self, event=None):
+        self.state = not self.state
+        self.tk.attributes("-fullscreen", self.state)
+        return "break"
+        
+    def end_fullscreen(self, event=None):
+        self.state = False
+        self.tk.attributes("-fullscreen", False)
+        return "break"
+        
+
+        
+
+def main(args):
+    
+    connection = sqlite3.connect("DBSystem.sqlite3")
+    cursor = connection.cursor()
+    
+    cursor.execute("CREATE TABLE IF NOT EXISTS Contenedores(ContenedorId integer PRIMARY KEY AUTOINCREMENT,Nombre varchar(255))")
+    
+    initialInfo = ["Contenedor 1","Contenedor 2","Contenedor 3","Contenedor 4","Contenedor 5","Contenedor 6"]
+
+    for info in initialInfo:
+        cursor.execute("INSERT INTO Contenedores(Nombre) VALUES ('{}')".format(info))
+
+    connection.commit()
+    
+    root = FullScreenWindow()
+
+
+    connection.close()
+    root.tk.mainloop()
+
+
+    
+    
+
+if __name__ == '__main__':
+    import sys
+    sys.exit(main(sys.argv))
